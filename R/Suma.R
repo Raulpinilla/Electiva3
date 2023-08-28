8e05c40d40c0dee5420ca02e9a310154b5e7357b
Raulpinilla <raula.pinillam@ecci.edu.com>
2023-08-27 19:13:29 -0500

    Package Documentation Version 1

---
+++ R/Suma.R
@@ -0,0 +1,10 @@
+#' Funcion para sumar dos numeros
+#' @param x numero (entero o doble)
+#' @param y numero (entero o doble)
+#' @return El resultado de la suma 'x' + 'y'
+#' @example
+#' suma (2,3)
+#' suma (12.5, 34.7)
+suma <- function(x,y){
+  return(x+y)
+}
